8b2ada51e4fd432663b460b2dd8040b6ac2c518d
Marcos Vinicius <vsmarcos.15@gmail.com>
2023-09-05 18:35:00 -0300

    Implementando movimentos do rei, imprimindo os movimentos possiveis

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,10 +8,21 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
+    private int turn;
+    private Color currentPlayer;
     
     public ChessMatch(){
         board = new Board(8,8);
+        turn =1;
+        currentPlayer = Color.WHITE;
         initialSetup();
+    }
+    
+    public int getTurn(){
+        return turn;
+    }
+    public Color getCurrentPlayer(){
+        return currentPlayer;
     }
     public ChessPiece[][] getPieces(){
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getRows()];
@@ -22,12 +33,20 @@ public class ChessMatch {
         }
         return mat;
     }
+    
+    public boolean [][] possibleMoves(ChessPosition sourcePosition){
+        Position position = sourcePosition.toPosition();
+        validateSourcePosition(position);
+        return board.piece(position).possibleMoves();
+    }
+    
     public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
         validateTargetPosition(source,target);
         Piece capturedPiece = makeMove(source,target);
+        nextTurn();
         return (ChessPiece) capturedPiece;
         
     }
@@ -42,6 +61,9 @@ public class ChessMatch {
         if(!board.thereIsAPiece(position)){
             throw new ChessException("Nao existe peca na posicao desejada");
         }
+        if(currentPlayer!=((ChessPiece)board.piece(position)).getColor()){
+            throw new ChessException("A peca escolhida nao é sua");
+        }
         if(!board.piece(position).isThereAnyPossibleMove()){
             throw new ChessException("Nao existe movimentos possiveis para a peca escolhida");
         }
@@ -51,7 +73,11 @@ public class ChessMatch {
             throw new ChessException("A peca escolhida nao pode mover pra posicao de destino");
         }
     }
-        
+    
+    private void nextTurn(){
+        turn++;
+        currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }    
     
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column,row).toPosition());
@@ -59,6 +85,7 @@ public class ChessMatch {
     
     
     private void initialSetup(){
+         
         placeNewPiece('c', 2, new Rook(Color.WHITE,board));
         placeNewPiece('d', 1, new King(Color.WHITE,board));
 
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -13,10 +14,58 @@ public class King extends ChessPiece {
     public String toString(){
         return "K";
     }
+    private boolean canMove(Position position){
+       ChessPiece p = (ChessPiece)getBoard().piece(position);
+       return p == null || p.getColor() != getColor();
+    }
+    
 
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] mat =  new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0,0);
+        
+        //Acima
+        p.setValues(position.getRow()-1, position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Abaixo
+        p.setValues(position.getRow()+1, position.getColumn());
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Para direita
+        p.setValues(position.getRow(), position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Para esquerda
+        p.setValues(position.getRow(), position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Para diagonal esquerda e cima
+        p.setValues(position.getRow()-1, position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Para diagonal esquerda e baixo
+        p.setValues(position.getRow()+1, position.getColumn()-1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Para diagonal direita e cima
+        p.setValues(position.getRow()-1, position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        //Para diagonal direita e cima
+        p.setValues(position.getRow()+1, position.getColumn()+1);
+        if(getBoard().positionExists(p) && canMove(p)){
+            mat[p.getRow()][p.getColumn()]= true;
+        }
+        
         return mat;
     }
 }
